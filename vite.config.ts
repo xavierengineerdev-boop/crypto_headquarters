ae9e142f4f25d40436c6208a4522c34337753a0a
ihorhnennyi <ihorhnennyi@gmail.com>
2026-01-29 20:31:03 +0200

    update fornt

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
 	base: '/',
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': '/src',
-			'@assets': '/src/assets',
-			'@components': '/src/components',
-			'@config': '/src/config',
-			'@hooks': '/src/hooks',
-			'@layouts': '/src/layouts',
-			'@pages': '/src/pages',
-			'@router': '/src/router',
-			'@store': '/src/store',
-			'@styles': '/src/styles',
-			'@types': '/src/types',
-			'@utils': '/src/utils',
-			'@fonts': '/src/fonts',
-			'@api': '/src/api',
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@config': path.resolve(__dirname, './src/config'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@layouts': path.resolve(__dirname, './src/layouts'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@router': path.resolve(__dirname, './src/router'),
+			'@store': path.resolve(__dirname, './src/store'),
+			'@styles': path.resolve(__dirname, './src/styles'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@fonts': path.resolve(__dirname, './src/fonts'),
+			'@api': path.resolve(__dirname, './src/api'),
+		},
+	},
+	server: {
+		fs: {
+			strict: false,
 		},
 	},
 	build: {
